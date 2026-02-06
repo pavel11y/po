0ebc22098f76ed09dfe84ec2c0bd93f13d88a719
pavel11y <isip_p.a.podyablonsky@mpt.ru>
2026-02-06 11:16:17 +0300

    Update dart_application_2.dart

--- dart_application_2.dart
+++ dart_application_2.dart
@@ -2,10 +2,71 @@ import 'dart:io';
 import 'dart:math';
 
 void main(List <String> argouments){
+  print('Введите первое число');
+  var num = double.parse(stdin.readLineSync()!);
   
+  print('Введите второе число');
+  var num2 = double.parse(stdin.readLineSync()!);
+  
+  print('Введите оператор');
+  String op = stdin.readLineSync()!;
 
-  
-  
+  switch (op) {
+    case '+':
+      print(num + num2);
+      break;
+    case '-':
+      print(num - num2);
+      break;
+    case '*':
+      print(num * num2);
+      break;
+    case '/':
+      if (num2 == 0) {
+        print('Нельзя делить на 0');
+      } else {
+        print(num / num2);
+      }
+      break;
+    case '~/':
+      if (num2 == 0) {
+        print('Нельзя делить на 0');
+      } else {
+        print(num ~/ num2);
+      }
+      break;
+    case '%':
+      if (num2 == 0) {
+        print('Нельзя делить на 0');
+      } else {
+        print(num % num2);
+      }
+      break;
+    case '**':
+      print(pow(num, num2));
+      break;
+    case '==':
+      print(num == num2);
+      break;
+    case '!=':
+      print(num != num2);
+      break;
+    case '>':
+      print(num > num2);
+      break;
+    case '<':
+      print(num < num2);
+      break;
+    case '>=':
+      print(num >= num2);
+      break;
+    case '<=':
+      print(num <= num2);
+      break;
+    default:
+      print('Неизвестный оператор');
+      break;
+  }
 
   bool b1 = bool.parse(stdin.readLineSync()?.toLowerCase() ?? '');
   bool b2 = bool.parse(stdin.readLineSync()?.toLowerCase() ?? '');
